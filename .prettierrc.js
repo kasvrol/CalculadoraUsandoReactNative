a285de8ad08467db0071f59a78a15fd57893b207
ana.karolina <ana.karolina@ablsystem.com.br>
2023-01-10 11:16:19 -0300

    limpeza dos arquivos instalados

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: true,
+  tabWidth: 4,
 };
